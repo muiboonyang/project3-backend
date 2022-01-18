1cf74b0923635f46332e448eb3a5547ab8a2aac8
Ernest <muiboonyang@gmail.com>
2022-01-18 15:56:41 +0800

    Implemented login/logout!

--- models/users.js
+++ models/users.js
@@ -8,6 +8,12 @@ const UserSchema = new mongoose.Schema(
   {
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    contact: { type: Number, required: true },
+    address: { type: String, required: true },
+    unit: { type: String, required: true },
+    zipcode: { type: String, required: true },
   },
   { timestamps: true },
   { collection: "users" }
