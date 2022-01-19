13fc16ad2497828e0463fe06ce00aa7cc300da95
Ernest <muiboonyang@gmail.com>
2022-01-19 23:55:30 +0800

    edit seed tasks and users

--- models/seed-tasks.js
+++ models/seed-tasks.js
@@ -1,49 +1,57 @@
 module.exports = [
   {
-    username: "1",
+    username: "ernest",
     name: "Ernest",
     email: "ernest@outlook.sg",
     contact: "123456",
     address: "Sin Ming",
     unit: "11-11",
     zipcode: "556677",
-    type: "plumbing",
+    type: "homeservices",
     date: "01/01/2022",
-    title: "Title 1",
+    title: "Home movers",
     deadline: "01/17/2022",
     comments: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non dolor quam. Integer imperdiet est magna, non pulvinar quam ornare vitae. Duis varius orci quis risus gravida, id maximus augue egestas. Morbi gravida placerat maximus. In vel lacus a lectus pellentesque sagittis. Aenean id efficitur ligula. Suspendisse nunc libero, sodales in nisl eu, dictum vehicula libero. Fusce euismod vulputate risus vitae tempor. Vivamus consequat posuere nulla ac pellentesque.`,
-    deadline: "12/12/2022",
+    accepted: true,
+    completed: true,
+    acceptedBy: "2",
+    image: "https://picsum.photos/id/1026/4621/3070",
+    review: "Very helpful!",
   },
   {
-    username: "2",
+    username: "jengmun",
     name: "Jeng Mun",
     email: "jengmun@yahoo.com",
     contact: "123456",
     address: "Jurong",
     unit: "10-01",
     zipcode: "901132",
-    type: "cleaning",
+    type: "business",
     date: "01/13/2022",
-    title: "Title 2",
+    title: "Data entry",
     deadline: "01/17/2022",
     comments: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non dolor quam. Integer imperdiet est magna, non pulvinar quam ornare vitae. Duis varius orci quis risus gravida, id maximus augue egestas. Morbi gravida placerat maximus. In vel lacus a lectus pellentesque sagittis. Aenean id efficitur ligula. Suspendisse nunc libero, sodales in nisl eu, dictum vehicula libero. Fusce euismod vulputate risus vitae tempor. Vivamus consequat posuere nulla ac pellentesque.
 
 Phasellus interdum purus vitae libero convallis, in egestas ipsum pulvinar. In turpis nunc, aliquam quis varius nec, congue id ante. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Morbi elementum ligula id mauris iaculis pharetra. Cras nec ligula cursus, bibendum tellus eu, convallis lacus. Pellentesque vitae blandit ante. Quisque lorem nisl, vehicula vel dictum in, finibus eget nunc.
 
 Sed leo magna, fringilla ut scelerisque non, lobortis at urna. Sed dignissim, purus at rutrum vestibulum, massa velit egestas magna, ac feugiat risus nibh sit amet velit. Fusce ut blandit justo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus at viverra purus. Proin egestas facilisis ligula nec viverra. Cras vitae turpis id velit pellentesque interdum.`,
-    deadline: "1/12/2022",
+    accepted: false,
+    completed: false,
+    acceptedBy: "",
+    image: "https://picsum.photos/id/1029/367/267",
+    review: "",
   },
   {
-    username: "3",
+    username: "clement",
     name: "Clement",
     email: "clement@gmail.com",
     contact: "123456",
     address: "Clementi",
     unit: "11-32",
     zipcode: "123456",
-    type: "grocery",
+    type: "business",
     date: "12/01/2022",
-    title: "Title 3",
+    title: "Game design",
     deadline: "01/17/2022",
     comments: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non dolor quam. Integer imperdiet est magna, non pulvinar quam ornare vitae. Duis varius orci quis risus gravida, id maximus augue egestas. Morbi gravida placerat maximus. In vel lacus a lectus pellentesque sagittis. Aenean id efficitur ligula. Suspendisse nunc libero, sodales in nisl eu, dictum vehicula libero. Fusce euismod vulputate risus vitae tempor. Vivamus consequat posuere nulla ac pellentesque.
 
@@ -54,19 +62,23 @@ Sed leo magna, fringilla ut scelerisque non, lobortis at urna. Sed dignissim, pu
 Aliquam vestibulum lectus in urna scelerisque, placerat pretium nisl mollis. Aenean vel faucibus enim, vitae placerat quam. Praesent lobortis nisi sed eros feugiat feugiat. Fusce accumsan rutrum viverra. Donec aliquam interdum ante, at lobortis arcu. Vestibulum quis erat ut purus pulvinar dictum eget non ante. Quisque at mauris sapien.
 
 Cras eu venenatis nibh. Quisque rhoncus ullamcorper sem, in bibendum libero aliquam in. Donec blandit vulputate commodo. Vestibulum viverra arcu quis risus tristique, non facilisis libero viverra. Curabitur aliquam ipsum diam, et luctus nulla molestie nec. Sed urna arcu, iaculis eget purus sit amet, molestie elementum nisl. Mauris fermentum ut justo id dignissim. Ut mi lectus, ullamcorper a rhoncus sed, venenatis in leo. Duis pretium nulla est, non placerat elit viverra a. Sed quam sem, accumsan ut diam id, semper vulputate lectus. Praesent sed dolor mattis, consectetur odio ac, finibus justo. Sed ligula justo, venenatis quis elementum non, porta sit amet tortor.`,
-    deadline: "12/11/2022",
+    accepted: false,
+    completed: false,
+    acceptedBy: "",
+    image: "https://picsum.photos/id/1033/367/267",
+    review: "",
   },
   {
-    username: "4",
+    username: "alex",
     name: "Alex",
-    email: "ernest@outlook.sg",
+    email: "alex@outlook.sg",
     contact: "123456",
     address: "Sin Ming",
     unit: "11-11",
     zipcode: "556677",
-    type: "grocery",
+    type: "lifestyle",
     date: "12/01/2022",
-    title: "Title 4",
+    title: "Interview prep",
     deadline: "01/17/2022",
     comments: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non dolor quam. Integer imperdiet est magna, non pulvinar quam ornare vitae. Duis varius orci quis risus gravida, id maximus augue egestas. Morbi gravida placerat maximus. In vel lacus a lectus pellentesque sagittis. Aenean id efficitur ligula. Suspendisse nunc libero, sodales in nisl eu, dictum vehicula libero. Fusce euismod vulputate risus vitae tempor. Vivamus consequat posuere nulla ac pellentesque.
 
@@ -77,20 +89,24 @@ Sed leo magna, fringilla ut scelerisque non, lobortis at urna. Sed dignissim, pu
 Aliquam vestibulum lectus in urna scelerisque, placerat pretium nisl mollis. Aenean vel faucibus enim, vitae placerat quam. Praesent lobortis nisi sed eros feugiat feugiat. Fusce accumsan rutrum viverra. Donec aliquam interdum ante, at lobortis arcu. Vestibulum quis erat ut purus pulvinar dictum eget non ante. Quisque at mauris sapien.
 
 Cras eu venenatis nibh. Quisque rhoncus ullamcorper sem, in bibendum libero aliquam in. Donec blandit vulputate commodo. Vestibulum viverra arcu quis risus tristique, non facilisis libero viverra. Curabitur aliquam ipsum diam, et luctus nulla molestie nec. Sed urna arcu, iaculis eget purus sit amet, molestie elementum nisl. Mauris fermentum ut justo id dignissim. Ut mi lectus, ullamcorper a rhoncus sed, venenatis in leo. Duis pretium nulla est, non placerat elit viverra a. Sed quam sem, accumsan ut diam id, semper vulputate lectus. Praesent sed dolor mattis, consectetur odio ac, finibus justo. Sed ligula justo, venenatis quis elementum non, porta sit amet tortor.`,
-    deadline: "12/11/2022",
+    accepted: false,
+    completed: false,
+    acceptedBy: "",
+    image: "https://picsum.photos/id/1043/367/267",
+    review: "",
   },
 
   {
-    username: "5",
+    username: "weijie",
     name: "Wei Jie",
     email: "ernest@outlook.sg",
     contact: "123456",
     address: "Sin Ming",
     unit: "11-11",
     zipcode: "556677",
-    type: "grocery",
+    type: "lifestyle",
     date: "12/01/2022",
-    title: "Title 5",
+    title: "Learn Python",
     deadline: "01/17/2022",
     comments: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer non dolor quam. Integer imperdiet est magna, non pulvinar quam ornare vitae. Duis varius orci quis risus gravida, id maximus augue egestas. Morbi gravida placerat maximus. In vel lacus a lectus pellentesque sagittis. Aenean id efficitur ligula. Suspendisse nunc libero, sodales in nisl eu, dictum vehicula libero. Fusce euismod vulputate risus vitae tempor. Vivamus consequat posuere nulla ac pellentesque.
 
@@ -101,6 +117,10 @@ Sed leo magna, fringilla ut scelerisque non, lobortis at urna. Sed dignissim, pu
 Aliquam vestibulum lectus in urna scelerisque, placerat pretium nisl mollis. Aenean vel faucibus enim, vitae placerat quam. Praesent lobortis nisi sed eros feugiat feugiat. Fusce accumsan rutrum viverra. Donec aliquam interdum ante, at lobortis arcu. Vestibulum quis erat ut purus pulvinar dictum eget non ante. Quisque at mauris sapien.
 
 Cras eu venenatis nibh. Quisque rhoncus ullamcorper sem, in bibendum libero aliquam in. Donec blandit vulputate commodo. Vestibulum viverra arcu quis risus tristique, non facilisis libero viverra. Curabitur aliquam ipsum diam, et luctus nulla molestie nec. Sed urna arcu, iaculis eget purus sit amet, molestie elementum nisl. Mauris fermentum ut justo id dignissim. Ut mi lectus, ullamcorper a rhoncus sed, venenatis in leo. Duis pretium nulla est, non placerat elit viverra a. Sed quam sem, accumsan ut diam id, semper vulputate lectus. Praesent sed dolor mattis, consectetur odio ac, finibus justo. Sed ligula justo, venenatis quis elementum non, porta sit amet tortor.`,
-    deadline: "12/11/2022",
+    accepted: false,
+    completed: false,
+    acceptedBy: "",
+    image: "https://picsum.photos/id/1036/367/267",
+    review: "",
   },
 ];
